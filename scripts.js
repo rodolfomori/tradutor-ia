2bf43610e7cf9e7c1c6b51c0d586e80d55e6cf75
rodolfomori <rod_dolpho@hotmail.com>
2026-01-18 18:50:45 -0300

    aula 4

--- scripts.js
+++ scripts.js
@@ -18,10 +18,17 @@ JavaScript
     console.log() mostra o que eu quiser na tela
 
     [x] Saber quando o botão foi clicado
-    [ ] Pegar o texto que o usário digitou
-    [ ] Mando para o servidor traduzir
-    [ ] Receber a resposta do servidor (traducao)  
-    [ ] Colocar o texto na tela   
+    [x] Pegar o texto que o usário digitou
+    [x] Mando para o servidor traduzir
+    [x] Receber a resposta do servidor (traducao)  
+    [x] Colocar o texto na tela 
+    [x] Escolher o idioma
+      
+    Fase 2
+    [x] Saber quando o botão de microfone foi clicado 
+    [ ] IA - Detectar a voz e pegar a transcricao
+    [ ] Traduzir o que foi falado    
+        
 
     // JavaScript - scripts
     // HTML - document
@@ -37,20 +44,50 @@ JavaScript
    json (formato mais amigavel)
 */
 
+// pegando o texto dentro do text area
 let inputTexto = document.querySelector(".input-texto")
+let traducaoTexto = document.querySelector(".traducao")
+let idioma = document.querySelector(".idioma")
 
+async function traduzir() {
 
-async function traduzir(){
+    // endereco do servidor com o texto que eu quero traduzir
+    let endereco = "https://api.mymemory.translated.net/get?q="
+        + inputTexto.value
+        + "&langpair=pt-BR|"
+        + idioma.value
 
-    let endereco = "https://api.mymemory.translated.net/get?q=" 
-    + inputTexto.value
-    + "&langpair=pt-BR|en"
-
+    // resposta do servidor
     let resposta = await fetch(endereco)
 
+    // converto a resposta para um formato mais amigavel
     let dados = await resposta.json()
-    
-    console.log(dados)
+
+    traducaoTexto.textContent = dados.responseData.translatedText
+
+    // textContent = conteudo do texto
+}
+
+function ouvirVoz() {
+    // ferramenta de transcricao de audio
+    let voz = window.webkitSpeechRecognition
+
+    // Deixando ela PRONTA PARA USO   
+    let reconhecimentoVoz = new voz()
+
+    // Configurando a ferramenta
+    reconhecimentoVoz.lang = "pt-BR"
+
+    // Me avise quando ele terminou de transcrever a voz
+    reconhecimentoVoz.onresult = (evento) => {
+        let textoTranscricao = evento.results[0][0].transcript
+
+        inputTexto.textContent = textoTranscricao
+
+        traduzir()
+    }
+
+    reconhecimentoVoz.start()
 
 }
 // clicou no botao -> chama a funcao -> monto o enderco ->
